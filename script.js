827c654c0cebc8bf5490d8b6fa4068f31c0905a4
MHemsley <mark.hemsley@annafreud.org>
2017-01-29 19:20:04 +0000

    refractor

--- script.js
+++ script.js
@@ -9,38 +9,77 @@ $(function(){
   $('.arrow-left').click(function(){
     $('.carousel').carousel('prev');
   })
+
+  //add in the modal on click
+  /*
+    $('.card-image').on('click'){
+
+  }
+  */
+  var embedded
   //make api request
-  $.ajax({url:"https://photorankapi-a.akamaihd.net/customers/215757/media/recent?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.0", success: function(result){
-    var embedded = result.data._embedded;
-    var auth = "?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.0"
-    //var caro = []
-    //iterative over the images and put them into an array with html formatting
-    for(var key in embedded){
-      var image = embedded[key].images.thumbnail + auth
-        var htm = '<div class="card">' +
-                        '<div class="card-image">' +
-                          '<img = src=' + image + '/>' +
-                          '<span class="card-title">Card Title</span>'
-                        '</div>' +
-                  '</div>'
-        //caro.push(htm);
-        if(key < 6){
-            $("#carousel_container_one").append(htm);
-        }
-        else if(key < 12){
-            $("#carousel_container_two").append(htm);
-          }
-        else if(key < 18){
-            $("#carousel_container_three").append(htm);
-          }
-    }
-    //shove the html elements into the correct carousel slide, so that 6 to a page
+  // % isolate the variables for use and auth before call to make code more re-usable
+  var customerID  = "215757"
+  var auth = "?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.0"
+  var mediaQuery = "media/recent"
+  var QueryUrl = "https://photorankapi-a.akamaihd.net/customers/" + customerID + "/" + mediaQuery + auth;
+
+
+var users = [];
+//  $.ajax({url:"https://photorankapi-a.akamaihd.net/customers/215757"})
+var request = $.ajax({url:QueryUrl, success: function(result){
+     return embedded = result.data._embedded;
+
+
 
 
 
   }});
+function setPics(mediaJSON){
 
-  $('.carousel_pic').click(function(){
+  for(var key in mediaJSON){
+    var image = mediaJSON[key].images.thumbnail + auth
+    var words = mediaJSON[key].caption;
+    words = words.slice(0,140);
+    words = words + '...'
+    console.log(words);
+    var userIndex = (Number(key))
+      var htm = '<div class="card">' +
+                      '<div class="card-image">' +
+                        '<img = src=' + image + '/>' +
+                      '</div>' +
+                      //to add in username  '<span class="card-title></span>' +
+                      '<div class="card-content">' +  words + ' </div>'
 
-  })
+                '</div>'
+
+      if(key < 6){
+          $("#carousel_container_one").append(htm);
+      }
+      else if(key < 12){
+          $("#carousel_container_two").append(htm);
+        }
+      else if(key < 18){
+          $("#carousel_container_three").append(htm);
+        }
+  }
+}
+
+/*
+function getUsers(Json){
+  for (key in Json){
+  var sourceID = Json[key]._embedded.uploader._links.self.href
+  var userUrl = "https:" + sourceID + auth
+   var userReq = $.ajax({url:userUrl, success: function(result){
+    var user = result.data.name
+    //if sorting may change make into an key pair object
+    users.push(user);
+  }})
+
+}} */
+request.done(function(){
+    //getUsers(embedded);
+    setPics(embedded);
+})
+
 })
